5da348a9e2ff46b8f8d4726b9088a444390286cd
Hadar <hlandao@gmail.com>
2016-04-04 23:26:25 +0300

    clenaup

--- swagger.js
+++ swagger.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec
 
 function swaggerToTypeScript(swaggerYamlPath, outputPath, namespace) {
-  console.log('swaggerToTypeScript',swaggerToTypeScript)
+  console.log("Running swagger-to-type-script for " + swaggerYamlPath);
   var child = exec('/usr/bin/java -jar '+__dirname+'/swagger-codegen-cli.jar generate -i '+swaggerYamlPath+'  -l typescript-angular -o '+outputPath+' --model-package '+namespace+' --api-package '+namespace,
     function (error, stdout, stderr){
       console.log('stdout: ' + stdout);
