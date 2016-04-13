f233739e5e955aa3c32afa6c41e674841f560670
Hadar <hlandao@gmail.com>
2016-04-13 15:15:42 +0300

    added option to specifiy the jar path

--- swagger.js
+++ swagger.js
@@ -1,8 +1,8 @@
 var exec = require('child_process').exec
 
-function swaggerToTypeScript(swaggerYamlPath, outputPath, namespace) {
-  console.log("Running swagger-to-type-script for " + swaggerYamlPath);
-  var child = exec('/usr/bin/java -jar '+__dirname+'/swagger-codegen-cli.jar generate -i '+swaggerYamlPath+'  -l typescript-angular -o '+outputPath+' --model-package '+namespace+' --api-package '+namespace,
+function swaggerToTypeScript(swaggerYamlPath, outputPath, namespace, pathToJar) {
+  pathToJar = pathToJar || (__dirname+'/swagger-codegen-cli.jar');
+  var child = exec('/usr/bin/java -jar ' + pathToJar + ' generate -i '+swaggerYamlPath+'  -l typescript-angular -o '+outputPath+' --model-package '+namespace+' --api-package '+namespace,
     function (error, stdout, stderr){
       console.log('stdout: ' + stdout);
       console.log('stderr: ' + stderr);
