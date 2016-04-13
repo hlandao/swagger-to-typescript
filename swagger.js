3d2ed59df1a30a6a34925c5775ec2388abd9a6ac
Hadar <hlandao@gmail.com>
2016-04-13 19:46:58 +0300

    updated the swagger cli jar

--- swagger.js
+++ swagger.js
@@ -1,7 +1,7 @@
 var exec = require('child_process').exec
 
 function swaggerToTypeScript(swaggerYamlPath, outputPath, namespace, pathToJar, done) {
-  pathToJar = pathToJar || (__dirname+'/swagger-codegen-cli-meteor.jar');
+  pathToJar = pathToJar || (__dirname+'/swagger-codegen-cli.jar');
   var child = exec('/usr/bin/java -jar ' + pathToJar + ' generate -i '+swaggerYamlPath+'  -l typescript-meteor -o '+outputPath+' --model-package '+namespace+' --api-package '+namespace,
     function (error, stdout, stderr){
       console.log('stdout: ' + stdout);
