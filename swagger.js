402f6444acd94fe4d19015acd783168214764e23
Hadar <hlandao@gmail.com>
2016-04-17 10:50:23 +0300

    fix bug

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ var execSync = require('sync-exec');
 function swaggerToTypeScript(swaggerYamlPath, outputPath, namespace, pathToJar, done) {
   pathToJar = pathToJar || (__dirname + '/swagger-codegen-cli.jar');
   var child = execSync('/usr/bin/java -jar ' + pathToJar + ' generate -i '+swaggerYamlPath+'  -l typescript-meteor -o '+outputPath+' --model-package '+namespace+' --api-package '+namespace);
-  done && done (error);
+  done && done ();
 }
 
 module.exports = swaggerToTypeScript
