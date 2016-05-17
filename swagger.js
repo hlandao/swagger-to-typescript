288365a08a2bfcaa3b263864a7055d48ca11f07e
Dotan Simha <dotansimha@gmail.com>
2016-05-17 14:10:53 +0300

    Handeling generation errors

--- swagger.js
+++ swagger.js
@@ -2,8 +2,24 @@ var execSync = require('sync-exec');
 
 function swaggerToTypeScript(swaggerYamlPath, outputPath, namespace, pathToJar, done) {
   pathToJar = pathToJar || (__dirname + '/swagger-codegen-cli.jar');
-  var child = execSync('/usr/bin/java -jar ' + pathToJar + ' generate -i '+swaggerYamlPath+'  -l typescript-meteor -o '+outputPath+' --model-package '+namespace+' --api-package '+namespace);
-  done && done ();
+  var command = '/usr/bin/java -jar ' + pathToJar + ' generate -i ' + swaggerYamlPath + '  -l typescript-meteor -o ' + outputPath + ' --model-package ' + namespace + ' --api-package ' + namespace;
+
+  var child = execSync(command);
+
+  if (child && child.status && child.status === 0) {
+    done && done();
+  }
+  else if (child && child.status && child.status === 1) {
+    throw "Unable to compile Swagger YAML file: " + swaggerYamlPath + " due to generator errors: \n " + child.stderr;
+  }
+  else if (child && child.status && child.status === 130) {
+    throw "Unable to compile Swagger YAML file due user interaption command, please run again!";
+  }
+  else {
+    console.log(child.stderr);
+    throw "Unable to compile Swagger YAML file due to error: ";
+  }
+
 }
 
-module.exports = swaggerToTypeScript
+module.exports = swaggerToTypeScript;
